637cbb6fa422d5452367a955546c3d0e83114f1f
caitlincorrigan <caitlinfcorrigan@gmail.com>
2024-02-21 17:04:32 -0800

    imports the auth, trip, and db blueprints and creates their py files

--- wndrlst/__init__.py
+++ wndrlst/__init__.py
@@ -1,7 +1,48 @@
+import os
+
 from flask import Flask
 
-app = Flask(__name__)
+def create_app(test_config=None):
+    # Create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+        # __name__ is the current Python module
+        # inst_rel_config means the config files are rel to the instance folder
+    # Def config settings for the app:
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+    if test_config is None:
+        # Load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # Load the test config if passed in
+        app.config.from_mapping(test_config)
+    
+    # Ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # A route and simple page that says 'hello'
+    @app.route('/hello')
+    def hello():
+        return 'Hello, world!'
+    
+    # Import the db file used to manage the database
+    from . import db
+    # Initialize the database
+    db.init_app(app)
+
+    # Import and register the auth blueprint
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    # Import and register the trip blueprint
+    from . import trip
+    app.register_blueprint(trip.bp)
+    app.add_url_rule('/', endpoint='index')
+    
+    return app
